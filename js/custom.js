5f877ba17a0a7f8d77b8691a432ded4e8268251c
gauravbluff <gaurav.kumarcsk@gmail.com>
2021-11-29 23:37:45 +0530

    new gimsh

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    autoplay:true,
+    animateOut: 'fadeOut',
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
